cf375c0ca2b39e83c1bdfa1425bde3912b4a9872
adriviris <adrianacarrenog@gmail.com>
2023-02-15 09:32:23 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+http.createServer((request, response) =>
+{
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Hello Node!\n');
+}).listen(8080);
+
+
+console.log('My first Node Server is Running on Port 8080.');
