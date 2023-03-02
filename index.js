fbe1e7f699c10ce83599af0746986f71f629b787
adriviris <adrianacarrenog@gmail.com>
2023-03-02 13:26:33 -0500

    static files

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const app = express();
-
-// const http = require ('http');
-// url = require('url');
+morgan = require('morgan');
+fs = require('fs'), //import built in node modules fs and path
+path = require('path');
 
 //LIST OF TOP 10 SUPER-HERO MOVIES
 let topMovies = [
@@ -58,7 +57,18 @@ let topMovies = [
     },
 ];
 
-/* http.createServer((request, response) => {
+const app = express();
+// create a write stream (in append mode)
+// a ‘log.txt’ file is created in root directory
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags : 'a'})
+
+// set up the logger
+app.use (morgan ('combined', {stream: accessLogStream}));
+
+/* const http = require ('http');
+url = require('url');
+
+    http.createServer((request, response) => {
     let requestURL = url.parse(request.url, true);
     if ( requestURL.pathname == '/documentation.html') {
     response.writeHead(200, {'Content-Type': 'text/plain'});
@@ -69,17 +79,24 @@ let topMovies = [
 }
 }).listen(8080); */
 
+// STATIC FILES 
+app.use('/staticFiles', express.static('public'));
+
+
 //GET REQUEST
-app.get('/', (_req, res) => {
-    res.send('Thank you for visiting the Movie Guru!');
+app.get('/', (req, res) => {
+    res.send('Thank you for visiting myFlix!');
+    console.log('Thank you for visiting myFlix!');
 });
 
-app.get('/documentation', (_req, res) => {
+app.get('/documentation', (req, res) => {
     res.sendFile('public/documentation.html', {root: __dirname });
 });
-app.get('/movies', (_req, res) => {
+app.get('/movies', (req, res) => {
     res.json(topMovies);
+    console.log('Top 10 Superhero Movies');
 });
+
 
 // LISTEN FOR REQUESTS
 
