d068abe2eef3725010f11d8d532af65f2a58834e
adriviris <adrianacarrenog@gmail.com>
2023-07-28 23:13:41 -0400

    auth.js - change passport

--- auth.js
+++ auth.js
@@ -3,11 +3,11 @@ const jwtSecret = 'your_jwt_secret'; // This has to be the same key used in the 
 const jwt = require ('jsonwebtoken'),
     passport = require('passport');
 
-require('./passport');
+require('./passport.js');
 // Your local passport file
 let generateJWTToken = (user) => {
     return jwt.sign(user, jwtSecret, {
-        subject: user.Username, // This is the username you’re encoding in the JWT
+        subject: user.UserName, // This is the username you’re encoding in the JWT
         expiresIn: '7d', // This specifies that the token will expire in 7 days
         algorithm: 'HS256' // This is the algorithm used to “sign” or encode the values of the JWT
     });
